3f6687737dc653ed8c7aab5c51fc7c1998794636
Lee Blue <lee@lee.blue>
2017-06-28 12:59:47 -0400

    Load scripts on all post types by default

--- includes/admin/settings/class-cc-admin-settings-post-type-selector.php
+++ includes/admin/settings/class-cc-admin-settings-post-type-selector.php
@@ -14,6 +14,13 @@ class CC_Admin_Settings_Post_Type_Selector extends CC_Admin_Settings_Checkboxes 
         }
 
         $selected_post_types = CC_Admin_Setting::get_option( 'cart66_main_settings', 'product_post_types' );
+
+        // If selected post types have not been saved yet, default to all post types
+        $main_settings_exist = get_option( 'cart66_main_settings');
+        if ( ! $main_settings_exist ) {
+            $selected_post_types = $post_types;
+        }
+
         $selected_post_types = is_array( $selected_post_types ) ? $selected_post_types : array();
         $selected_post_types[] = 'cc_product';
         $this->set_selected( $selected_post_types );
