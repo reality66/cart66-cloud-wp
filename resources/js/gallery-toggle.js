1a1f2e1bc0f4ec2533a688040e2f6d9e65fb7532
Lee Blue <lee@lee.blue>
2018-02-05 14:21:23 -0500

    Flip gallery images as product option changes.

--- resources/js/gallery-toggle.js
+++ resources/js/gallery-toggle.js
@@ -7,4 +7,24 @@ jQuery(document).ready(function($) {
         $('#' + ref).show();
     });
 
+    $(document).on( 'change', 'select.cart66-switch-image', function( event ) {
+    	var index = $(this).find(':selected').data('index');
+
+    	if ( $('.cc-image-index-' + index)[0] ) {
+    		$('.cc-gallery-full-image').hide();
+    		$('.cc-image-index-' + index).show();
+    	}
+    	
+    });
+
+    $(document).on( 'change', 'input[type=radio].cart66-switch-image', function( event ) {
+    	var index = $(this).data('index');
+
+    	if ( $('.cc-image-index-' + index)[0] ) {
+    		$('.cc-gallery-full-image').hide();
+    		$('.cc-image-index-' + index).show();
+    	}
+    	
+    });
+
 });
